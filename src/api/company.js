d1a0203565b09172f33f65280b1553d6b82c3220
kongxin <1095716578@qq.com>
2021-08-23 10:47:26 +0800

    fix:还原代码

--- src/api/company.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 通过公司名字进行模糊查询
-export function searchCompany(params) {
-  return request({
-    url: `/user/selectNameVague/${params}`,
-    method: 'get'
-  })
-}
