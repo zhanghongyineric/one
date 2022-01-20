c9fa0166f46e83ddc93e936ac8dc82753588fb56
kongxin <1095716578@qq.com>
2022-01-20 11:45:28 +0800

    fix:首页趋势数据判空

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -531,10 +531,12 @@ export default {
         .then(res => {
           const { data } = res
           const arr = new Array(12).fill(0)
-          data.forEach(v => {
-            arr[parseInt(v.month - 1)] = v.count
-          })
-          this.trendData = arr
+          if (data) {
+            data.forEach(v => {
+              arr[parseInt(v.month - 1)] = v.count
+            })
+            this.trendData = arr
+          } else this.trendData = []
         })
         .catch(err => {
           throw err
